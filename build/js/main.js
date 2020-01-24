e1a4ba287e29db9f705268d56ed92a1cdd494084
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2020-01-24 15:33:49 +0300

    Lesson 2.0

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,19 @@
 "use strict";
 
-var money, income, addExpenses, deposit, mission, period;
-alert('Hello!');
-console.log('007');
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+var money = 50000,
+    income = "Фриланс",
+    addExpenses = "Кофе, Интернет, Такси",
+    deposit = false,
+    mission = 300000,
+    period = 12;
+console.log(_typeof(money));
+console.log(_typeof(income));
+console.log(_typeof(deposit));
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+console.log(addExpenses.toLowerCase());
+var budgetDay = money / 30;
+console.log(budgetDay);
--- dev/js/main.js
+++ dev/js/main.js
@@ -1,12 +1,23 @@
 "use strict";
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 50000,
+    income = "Фриланс",
+    addExpenses = "Кофе, Интернет, Такси",
+    deposit = false,
+    mission = 300000,
+    period = 12;
 
-alert('Hello!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('007');
+console.log(addExpenses.length);
+
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+
+
+console.log(addExpenses.toLowerCase());
+
+let budgetDay = money / 30;
+console.log(budgetDay);
