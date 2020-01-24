d2278bd44ccce4919b7b2d93e7ec3b1b735bd750
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2020-01-24 16:46:27 +0300

    Lesson 2.0: Правки

--- build/js/main.js
+++ build/js/main.js
@@ -15,5 +15,6 @@ console.log(addExpenses.length);
 console.log("Период равен " + period + " месяцев");
 console.log("Цель заработать " + mission + " рублей");
 console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
 var budgetDay = money / 30;
 console.log(budgetDay);
