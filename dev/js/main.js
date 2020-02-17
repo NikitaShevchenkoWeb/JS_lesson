1f70ceb07048b62df94c897cbf7150a39fd6a89d
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2020-02-17 22:02:30 +0300

    Lesson 17

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,23 +1,48 @@
-"use strict";
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    let count = 0;
+    
+    //Timer
+    function countTimer(deadline) {
+        const timerHours = document.querySelector('#timer-hours'),
+              timerMinutes = document.querySelector('#timer-minutes'),
+              timerSeconds = document.querySelector('#timer-seconds');
 
-let money = 50000,
-    income = "Фриланс",
-    addExpenses = "Кофе, Интернет, Такси",
-    deposit = false,
-    mission = 300000,
-    period = 12;
+        function getTimeRemaining() {
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+            return {timeRemaining, hours, minutes, seconds};
+        }
 
-console.log(addExpenses.length);
+        function updateClock() {
+            count = 1000;
 
-console.log("Период равен " + period + " месяцев");
-console.log("Цель заработать " + mission + " рублей");
+            let timer = getTimeRemaining();
 
+            timerHours.textContent = newChar(timer.hours);
+            timerMinutes.textContent = newChar(timer.minutes);
+            timerSeconds.textContent = newChar(timer.seconds);
 
-console.log(addExpenses.toLowerCase());
+            if (timer.timeRemaining <= 0) {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
 
-let budgetDay = money / 30;
-console.log(budgetDay);
+                clearInterval(idInterval);
+            }
+        }
+
+        function newChar(elem) {
+            return elem < 10 ? "0" + elem : elem;
+        }
+
+        let idInterval = setInterval(updateClock, count);
+    }
+
+    countTimer('30 February 2020');
+});
