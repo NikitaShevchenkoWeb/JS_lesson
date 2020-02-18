0ed697a87cf20a1fcda4571b1e2827ec7f385bb1
Nikita Shevchenko <nikita_shevchenko@inbox.ru>
2020-02-18 15:33:09 +0300

    Lesson 17: Hard(1)

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,23 +1,52 @@
-"use strict";
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    let count = 0;
+    
+    //Timer
+    function countTimer(deadline) {
+        let deadLine = deadline;
 
-let money = 50000,
-    income = "Фриланс",
-    addExpenses = "Кофе, Интернет, Такси",
-    deposit = false,
-    mission = 300000,
-    period = 12;
+        const timerHours = document.querySelector('#timer-hours'),
+              timerMinutes = document.querySelector('#timer-minutes'),
+              timerSeconds = document.querySelector('#timer-seconds');
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+        function getTimeRemaining() {
+            let dateStop = new Date(deadLine).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
 
-console.log(addExpenses.length);
+            return {timeRemaining, hours, minutes, seconds};
+        }
 
-console.log("Период равен " + period + " месяцев");
-console.log("Цель заработать " + mission + " рублей");
+        function updateClock() {
+            count = 1000;
 
+            let timer = getTimeRemaining();
 
-console.log(addExpenses.toLowerCase());
+            timerHours.textContent = newChar(timer.hours);
+            timerMinutes.textContent = newChar(timer.minutes);
+            timerSeconds.textContent = newChar(timer.seconds);
 
-let budgetDay = money / 30;
-console.log(budgetDay);
+            if (timer.timeRemaining <= 0) {
+                let newNumberDay = Number.parseInt(newChar(deadline).slice(0, 2)) + 1;
+                if (newNumberDay < 10) {newNumberDay += " "}
+                let month = deadline.substring(2);
+                deadLine = newNumberDay + month;
+
+                count = 0;
+                countTimer(deadLine);
+            }
+        }
+
+        function newChar(elem) {
+            return elem < 10 ? "0" + elem : elem;
+        }
+
+        setInterval(updateClock, count);
+    }
+
+    countTimer('18 February 2020');
+});
